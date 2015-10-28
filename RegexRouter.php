fe8dd699fc2da3424feabdc0c9b62971b0f8fd36
cischlomo <schlomo@campidiot.com>
2015-10-27 23:16:33 -0500

    separate funcs for get and post

--- RegexRouter.php
+++ RegexRouter.php
@@ -2,14 +2,19 @@
 
 class RegexRouter {
     
-    private $routes = array();
+  private $getroutes = array(), $postroutes=array();
     
-    public function route($pattern, $callback) {
-        $this->routes[$pattern] = $callback;
+  public function get ($pattern, $callback) {
+        $this->getroutes[$pattern] = $callback;
+    }
+
+    public function post ($pattern, $callback) {
+        $this->postroutes[$pattern] = $callback;
     }
     
     public function execute($uri) {
-        foreach ($this->routes as $pattern => $callback) {
+        foreach (($_SERVER['REQUEST_METHOD']==='POST' ? $this->postroutes : $this->getroutes)
+        as $pattern => $callback) {
             if (preg_match($pattern, $uri, $params) === 1) {
                 array_shift($params);
                 return call_user_func_array($callback, array_values($params));
